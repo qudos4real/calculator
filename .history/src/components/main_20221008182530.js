48aed05150a26de572a1d365364c328e6cdfb3d7
Qudus Badmus <qudos4real@yahoo.com>
2022-10-08 18:26:26 +0100

    restarting and adding an html boilerplate

---
+++ .history/src/components/main_20221008182530.js
@@ -0,0 +1,32 @@
+<div class="major-container">
+    <h3>A calculator created with Flexbox</h3>
+    <div class="container">
+        <div class="output"><h3>123</h3></div>
+        <div class="container-elements">
+            <div class="container2">
+                <button class="calc_btn number">1</button>
+                <button class="calc_btn number">2</button>
+                <button class="calc_btn number">3</button>
+                <button class="calc_btn operator">+</button>
+            </div>
+            <div class="container2">
+                <button class="calc_btn number">4</button>
+                <button class="calc_btn number">5</button>
+                <button class="calc_btn number">6</button>
+                <button class="calc_btn operator">-</button>
+            </div>
+            <div class="container2">
+                <button class="calc_btn number">7</button>
+                <button class="calc_btn number">8</button>
+                <button class="calc_btn number">9</button>
+                <button class="calc_btn operator">*</button>
+            </div>
+            <div class="container2">
+                <button class="calc_btn cancel">C</button>
+                <button class="calc_btn number">0</button>
+                <button class="calc_btn number">=</button>
+                <button class="calc_btn operator">/</button>
+            </div>
+        </div>
+    </div>
+</div>
