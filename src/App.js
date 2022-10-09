dfaedc3c9c0f9a62ba5d5d6370160df5741fa912
Qudus Badmus <qudos4real@yahoo.com>
2022-10-09 03:02:29 +0100

    made screen reflect numbers, - redundant files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import Buttons from "./components/Buttons";
-import Screen from "./components/Screen";
+import Buttons from "./components/Calculator";
 
 
-const numbers = [1,2,3,"+"]
 
-
-const numbers = [
-  [1, 2, 3, "+"],
-  [4, 5, 6, "-"],
-  [7, 8, 9, "*"],
-  ["C", 0, "=", "/"],
-];
 
 function App() {
   return (
-    <div>
-     <Main />
-     <Buttons />
-    </div>
+    <>
+         <Buttons />
+    </>
   );
 }
 
